071503a8a5803cbf643700a406e4d654707e31de
TheFrenchGhosty <TheFrenchGhosty@posteo.is>
2020-06-12 19:59:24 +0200

    Add Audio Only scripts for Windows

---
+++ Windows/Audio Only Script/Playlists/Playlists.ps1
@@ -0,0 +1,9 @@
+#
+#
+# TheFrenchGhostys YouTube-DL Archivist Scripts: The ultimate collection of scripts for YouTube-DL
+# https://github.com/TheFrenchGhosty/TheFrenchGhostys-YouTube-DL-Archivist-Scripts
+# https://github.com/TheFrenchGhosty
+#
+#
+
+youtube-dl --format "(bestaudio[acodec=opus]/bestaudio)/best" --verbose --force-ipv4 --sleep-interval 5 --max-sleep-interval 30 --ignore-errors --no-continue --no-overwrites --download-archive archive.log --add-metadata --write-description --write-info-json --write-annotations --write-thumbnail --embed-thumbnail --extract-audio --output "%(playlist)s - (%(uploader)s)/%(upload_date)s - %(title)s/%(upload_date)s - %(title)s [%(id)s].%(ext)s" --merge-output-format "mkv" --batch-file "Source - Playlists.txt"
