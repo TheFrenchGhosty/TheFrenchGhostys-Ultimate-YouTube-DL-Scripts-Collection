c9732fbf8c807004b0bea6e7d627b3f795ad14c0
TheFrenchGhosty <TheFrenchGhosty@posteo.is>
2020-06-15 20:16:37 +0200

    Enhancement of --format + Full fix of the Watch on Mobile Devices scripts (Closes #9) - Thanks to po5 for the help

--- Windows/Audio Only Script/Listen/Listen.ps1
+++ Windows/Audio Only Script/Listen/Listen.ps1
@@ -6,4 +6,4 @@
 #
 #
 
-youtube-dl --format "(bestaudio[acodec=opus]/bestaudio)/best" --verbose --force-ipv4 --sleep-interval 5 --max-sleep-interval 30 --ignore-errors --no-continue --no-overwrites --download-archive archive.log --add-metadata --extract-audio --output "%(uploader)s - %(upload_date)s - %(title)s [%(id)s].%(ext)s" --merge-output-format "mkv" --batch-file "Source - Listen.txt"
+youtube-dl --format "(bestaudio[acodec^=opus]/bestaudio)/best" --verbose --force-ipv4 --sleep-interval 5 --max-sleep-interval 30 --ignore-errors --no-continue --no-overwrites --download-archive archive.log --add-metadata --extract-audio --output "%(uploader)s - %(upload_date)s - %(title)s [%(id)s].%(ext)s" --merge-output-format "mkv" --batch-file "Source - Listen.txt"
