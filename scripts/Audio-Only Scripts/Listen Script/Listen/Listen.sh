688ebcda9c21cedf799fdadb8acaeb6d17c74161
TheFrenchGhosty <TheFrenchGhosty@posteo.is>
2021-02-17 03:16:07 +0100

    Output stdout and stderr to output.log

--- scripts/Audio-Only Scripts/Listen Script/Listen/Listen.sh
+++ scripts/Audio-Only Scripts/Listen Script/Listen/Listen.sh
@@ -6,4 +6,4 @@
 #
 #
 
-youtube-dlc --format "(bestaudio[acodec^=opus]/bestaudio)/best" --verbose --force-ipv4 --sleep-interval 5 --max-sleep-interval 30 --ignore-errors --no-continue --no-overwrites --download-archive archive.log --add-metadata --extract-audio --output "%(uploader)s - %(upload_date)s - %(title)s [%(id)s].%(ext)s" --merge-output-format "mkv" --batch-file "Source - Listen.txt"
+youtube-dlc --format "(bestaudio[acodec^=opus]/bestaudio)/best" --verbose --force-ipv4 --sleep-interval 5 --max-sleep-interval 30 --ignore-errors --no-continue --no-overwrites --download-archive archive.log --add-metadata --extract-audio --output "%(uploader)s - %(upload_date)s - %(title)s [%(id)s].%(ext)s" --merge-output-format "mkv" --batch-file "Source - Listen.txt" 2>&1 | tee output.log
