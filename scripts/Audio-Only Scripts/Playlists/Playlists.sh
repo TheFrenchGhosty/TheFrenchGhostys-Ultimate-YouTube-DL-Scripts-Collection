30958d3dbfad89580bdedf2bf5ffcf7dc92922bf
TheFrenchGhosty <TheFrenchGhosty@posteo.is>
2021-02-17 02:41:17 +0100

    Skip livestreams currently running in every scripts except Watch and Listen (Closes #34)

--- scripts/Audio-Only Scripts/Playlists/Playlists.sh
+++ scripts/Audio-Only Scripts/Playlists/Playlists.sh
@@ -6,4 +6,4 @@
 #
 #
 
-youtube-dlc --format "(bestaudio[acodec^=opus]/bestaudio)/best" --verbose --force-ipv4 --sleep-interval 5 --max-sleep-interval 30 --ignore-errors --no-continue --no-overwrites --download-archive archive.log --add-metadata --write-description --write-info-json --write-annotations --write-thumbnail --embed-thumbnail --extract-audio --output "%(playlist)s - (%(uploader)s)/%(upload_date)s - %(title)s/%(upload_date)s - %(title)s [%(id)s].%(ext)s" --merge-output-format "mkv" --batch-file "Source - Playlists.txt"
+youtube-dlc --format "(bestaudio[acodec^=opus]/bestaudio)/best" --verbose --force-ipv4 --sleep-interval 5 --max-sleep-interval 30 --ignore-errors --no-continue --no-overwrites --download-archive archive.log --add-metadata --write-description --write-info-json --write-annotations --write-thumbnail --embed-thumbnail --extract-audio --match-filter "!is_live & !live" --output "%(playlist)s - (%(uploader)s)/%(upload_date)s - %(title)s/%(upload_date)s - %(title)s [%(id)s].%(ext)s" --merge-output-format "mkv" --batch-file "Source - Playlists.txt"
