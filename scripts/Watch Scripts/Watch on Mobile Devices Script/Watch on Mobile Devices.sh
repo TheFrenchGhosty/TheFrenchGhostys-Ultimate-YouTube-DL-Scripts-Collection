e371d3ffa617880b0aa2241dcdfefa886e141c1d
TheFrenchGhosty <TheFrenchGhosty@posteo.is>
2022-02-28 18:37:58 +0100

    Version 3.2.1

--- scripts/Watch Scripts/Watch on Mobile Devices Script/Watch on Mobile Devices.sh
+++ scripts/Watch Scripts/Watch on Mobile Devices Script/Watch on Mobile Devices.sh
@@ -4,7 +4,7 @@
 # https://github.com/TheFrenchGhosty/TheFrenchGhostys-Ultimate-YouTube-DL-Scripts-Collection
 # https://github.com/TheFrenchGhosty
 #
-# Version: 3.2.0
+# Version: 3.2.1
 #
 
 yt-dlp --format "(bestvideo[vcodec^=avc1][height=1080][fps>30]/bestvideo[vcodec^=avc1][height=1080]/bestvideo[vcodec^=avc1][height=720][fps>30]/bestvideo[vcodec^=avc1][height=720]/bestvideo[vcodec^=avc1][height=480][fps>30]/bestvideo[vcodec^=avc1][height=480]/bestvideo[vcodec^=avc1][height=360][fps>30]/bestvideo[vcodec^=avc1][height=360]/bestvideo[vcodec^=avc1][height=240][fps>30]/bestvideo[vcodec^=avc1][height=240]/bestvideo[vcodec^=avc1][height=144][fps>30]/bestvideo[vcodec^=avc1][height=144]/bestvideo[vcodec^=avc1])+(bestaudio[acodec^=mp4a]/bestaudio)/best" --verbose --force-ipv4 --ignore-errors --no-continue --no-overwrites --download-archive archive.log --add-metadata --parse-metadata "%(title)s:%(meta_title)s" --parse-metadata "%(uploader)s:%(meta_artist)s" --all-subs --embed-subs --output "%(uploader)s - %(upload_date)s - %(title)s [%(id)s].%(ext)s" --merge-output-format "mkv" --throttled-rate 100K --batch-file "Source - Watch on Mobile Devices.txt" 2>&1 | tee output.log
