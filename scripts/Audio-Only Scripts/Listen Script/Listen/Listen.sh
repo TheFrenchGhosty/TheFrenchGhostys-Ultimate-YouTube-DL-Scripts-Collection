0069aad049bba9bbabd191e9139384628f5124b7
Player-5 <57650225+Player-5@users.noreply.github.com>
2022-04-23 20:39:13 -0400

    Reformatted All Scripts
    All scripts are now split across multiple lines instead of a single one.
    This should improve readability, and make future work easier.

--- scripts/Audio-Only Scripts/Listen Script/Listen/Listen.sh
+++ scripts/Audio-Only Scripts/Listen Script/Listen/Listen.sh
@@ -7,4 +7,24 @@
 # Version: 3.2.2
 #
 
-yt-dlp --format "(bestaudio[acodec^=opus]/bestaudio)/best" --verbose --force-ipv4 --sleep-requests 1 --sleep-interval 5 --max-sleep-interval 30 --ignore-errors --no-continue --no-overwrites --download-archive archive.log --add-metadata --parse-metadata "%(title)s:%(meta_title)s" --parse-metadata "%(uploader)s:%(meta_artist)s" --extract-audio --check-formats --concurrent-fragments 5 --output "%(uploader)s - %(upload_date)s - %(title)s [%(id)s].%(ext)s" --throttled-rate 100K --batch-file "Source - Listen.txt" 2>&1 | tee output.log
+yt-dlp \
+--format "(bestaudio[acodec^=opus]/bestaudio)/best" \
+--verbose \
+--force-ipv4 \
+--sleep-requests 1 \
+--sleep-interval 5 \
+--max-sleep-interval 30 \
+--ignore-errors \
+--no-continue \
+--no-overwrites \
+--download-archive archive.log \
+--add-metadata \
+--parse-metadata "%(title)s:%(meta_title)s" \
+--parse-metadata "%(uploader)s:%(meta_artist)s" \
+--extract-audio \
+--check-formats \
+--concurrent-fragments 5 \
+--output "%(uploader)s - %(upload_date)s - %(title)s [%(id)s].%(ext)s" \
+--throttled-rate 100K \
+--batch-file "Source - Listen.txt" 2>&1 \
+| tee output.log
