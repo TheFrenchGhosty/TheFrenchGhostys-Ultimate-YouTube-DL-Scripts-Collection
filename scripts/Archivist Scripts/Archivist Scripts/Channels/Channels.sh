cc4227cd2f4a7d9113ed29be83c38c10d67f8b70
Cook I.T! <55697931+Cook-I-T@users.noreply.github.com>
2023-12-18 20:06:48 +0100

    Add error handling
    Now checks if the file in the Formats folder exists and if the result of cat|sed|tr is not empty, if an error did occur it prints an error message and exits.
    This adds more lines but is still smaller than the archival formats and better readable overall.

--- scripts/Archivist Scripts/Archivist Scripts/Channels/Channels.sh
+++ scripts/Archivist Scripts/Archivist Scripts/Channels/Channels.sh
@@ -7,4 +7,16 @@
 # Version: 3.3.0
 #
 
-yt-dlp --format "$(cat "$(realpath "$(dirname "$0")/../../..")/Formats/best_formats.txt" | sed '/^#/d' | tr -d '\n')" --verbose --force-ipv4 --sleep-requests 1 --sleep-interval 5 --max-sleep-interval 30 --ignore-errors --no-continue --no-overwrites --download-archive archive.log --add-metadata --parse-metadata "%(title)s:%(meta_title)s" --parse-metadata "%(uploader)s:%(meta_artist)s" --write-description --write-info-json --write-annotations --write-thumbnail --embed-thumbnail --all-subs --embed-subs --get-comments --check-formats --concurrent-fragments 3 --match-filter "!is_live & !live" --output "%(uploader)s/%(uploader)s - %(upload_date)s - %(title)s/%(uploader)s - %(upload_date)s - %(title)s [%(id)s].%(ext)s" --merge-output-format "mkv" --datebefore "$(date --date="30 days ago" +%Y%m%d)" --throttled-rate 100K --batch-file "Source - Channels.txt" 2>&1 | tee output.log
+pathToFormat="$(realpath "$(dirname "$0")/../../..")/Formats/best_formats.txt"
+if ! [ -f "$pathToFormat" ]; then
+    echo "Error: File containing yt-dlp formats not found under \"$pathToFormat\"" >> /dev/stderr
+    exit 1
+fi
+
+convertedFormats=$(cat $pathToFormat | sed '/^#/d' | tr -d '\n')
+if [ -z "$convertedFormats" ]; then
+    echo "Error: Something went wrong when reading/transforming the content of the yt-dlp formats file." >> /dev/stderr
+    exit 1
+fi
+
+yt-dlp --format $convertedFormats --verbose --force-ipv4 --sleep-requests 1 --sleep-interval 5 --max-sleep-interval 30 --ignore-errors --no-continue --no-overwrites --download-archive archive.log --add-metadata --parse-metadata "%(title)s:%(meta_title)s" --parse-metadata "%(uploader)s:%(meta_artist)s" --write-description --write-info-json --write-annotations --write-thumbnail --embed-thumbnail --all-subs --embed-subs --get-comments --check-formats --concurrent-fragments 3 --match-filter "!is_live & !live" --output "%(uploader)s/%(uploader)s - %(upload_date)s - %(title)s/%(uploader)s - %(upload_date)s - %(title)s [%(id)s].%(ext)s" --merge-output-format "mkv" --datebefore "$(date --date="30 days ago" +%Y%m%d)" --throttled-rate 100K --batch-file "Source - Channels.txt" 2>&1 | tee output.log
--- scripts/Watch Scripts/Watch on Mobile Devices Script/Watch on Mobile Devices.sh
+++ scripts/Watch Scripts/Watch on Mobile Devices Script/Watch on Mobile Devices.sh
@@ -7,4 +7,16 @@
 # Version: 3.3.0
 #
 
-yt-dlp --format "$(cat "$(realpath "$(dirname "$0")/../..")/Formats/watch_mobile_formats.txt" | sed '/^#/d' | tr -d '\n')" --verbose --force-ipv4 --ignore-errors --no-continue --no-overwrites --download-archive archive.log --add-metadata --parse-metadata "%(title)s:%(meta_title)s" --parse-metadata "%(uploader)s:%(meta_artist)s" --all-subs --embed-subs --check-formats --concurrent-fragments 3 --output "%(uploader)s - %(upload_date)s - %(title)s [%(id)s].%(ext)s" --merge-output-format "mkv" --throttled-rate 100K --batch-file "Source - Watch on Mobile Devices.txt" 2>&1 | tee output.log
+pathToFormat="$(realpath "$(dirname "$0")/../..")/Formats/watch_mobile_formats.txt"
+if ! [ -f "$pathToFormat" ]; then
+    echo "Error: File containing yt-dlp formats not found under \"$pathToFormat\"" >> /dev/stderr
+    exit 1
+fi
+
+convertedFormats=$(cat $pathToFormat | sed '/^#/d' | tr -d '\n')
+if [ -z "$convertedFormats" ]; then
+    echo "Error: Something went wrong when reading/transforming the content of the yt-dlp formats file." >> /dev/stderr
+    exit 1
+fi
+
+yt-dlp --format $convertedFormats --verbose --force-ipv4 --ignore-errors --no-continue --no-overwrites --download-archive archive.log --add-metadata --parse-metadata "%(title)s:%(meta_title)s" --parse-metadata "%(uploader)s:%(meta_artist)s" --all-subs --embed-subs --check-formats --concurrent-fragments 3 --output "%(uploader)s - %(upload_date)s - %(title)s [%(id)s].%(ext)s" --merge-output-format "mkv" --throttled-rate 100K --batch-file "Source - Watch on Mobile Devices.txt" 2>&1 | tee output.log
